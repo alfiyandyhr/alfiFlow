5035ec94467cd00eddf3f3a7414a999a4cd679f4
alfiyandyhr <alfiyandyhariansyah@gmail.com>
2023-02-06 15:46:57 +0900

    added an example of von Neumann stability analysis

--- main_b.py
+++ main_b.py
@@ -13,9 +13,9 @@ analyses = [vonNeumannStabilityAnalysis(jmax=101) for i in range(5)]
 cfl_list = [0.25, 0.50, 0.75, 1.00, 1.25]
 
 for i, analysis in enumerate(analyses):
-	analysis.do(cfl=cfl_list[i], scheme="CentralDifference")
+	# analysis.do(cfl=cfl_list[i], scheme="CentralDifference")
 	# analysis.do(cfl=cfl_list[i], scheme="FirstOrderUpwindDifference")
-	# analysis.do(cfl=cfl_list[i], scheme="LaxScheme")
+	analysis.do(cfl=cfl_list[i], scheme="LaxScheme")
 	# analysis.do(cfl=cfl_list[i], scheme="LaxWendroffScheme")
 	# analysis.do(cfl=cfl_list[i], scheme="SecondOrderUpwindDifference")
 	# analysis.do(cfl=cfl_list[i], scheme="SecondOrderUpwindDifferenceModified")
